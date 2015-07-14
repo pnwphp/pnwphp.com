39f5f08d523be17d3ecdcb3278c79d862ce1d9d1
Andrew Woods <atwoods1@gmail.com>
2015-07-13 17:15:38 -0700

    Created 'session' view for the schedule page

---
+++ resources/views/session.blade.php
@@ -0,0 +1,10 @@
+
+<!-- the contents of a session  -->
+<div class="session">
+    <img src="{{ $speaker['avatar'] }}" alt="Photo of {{ $speaker['name'] }}"/>
+    <p>
+        <a href="#">{{ $speaker['name'] }}</a> <br/>
+        <a href="#" class="small">{{ $talk['title'] }}</a>
+    </p>
+</div>
+

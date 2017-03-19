6b9baf8aea68beb3af57200466eb75c6fcf3b8bb
Andrew Woods <atwoods1@gmail.com>
2017-03-19 15:55:39 -0700

    Organized routes into years
    The organization makes it easier to understand.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,6 +42,7 @@ $app->get('/sponsors', function() use ($app) {
  */
 
 
+
 /*
  * 2016 Site URLs
  */
@@ -53,15 +54,40 @@ $app->get('/2016/', function() use ($app) {
 $app->get('/2016/speakers', function() use ($app) {
     dryfta('speakers');
 });
+
 $app->get('/2016/schedule', function() use ($app) {
     dryfta('program-schedule');
 });
+
 $app->get('/2016/code-of-conduct', function() use ($app) {
     dryfta('code-of-conduct_1456806894');
 });
 
 $app->get('/2016/sponsors', function() use ($app) {
     dryfta('sponsors');
+});
+
+$app->get('/2016/schedule', function() use ($app) {
+    dryfta('schedule');
+});
+
+$app->get('/2016/events-hackathon', function() use ($app) {
+    dryfta('schedule');
+});
+
+$app->get('/2016/speakers', function() use ($app) {
+    // Redirect the 2015 url to 2016 page
+    dryfta('speakers');
+});
+
+$app->get('/2016/tags', function(Request $request) use ($app) {
+    // Redirect the 2015 url to 2016 page
+    dryfta('schedule');
+});
+
+$app->get('/2016/talks', function(Request $request) use ($app) {
+    // Redirect the 2015 url to 2016 page
+    dryfta('schedule');
 });
 
 
@@ -77,60 +103,32 @@ $app->get('jobs', function() use ($app) {
  */
 
 
-/*
-$app->get('sponsors', function() use ($app) {
-    dryfta('sponsors');
-});
-*/
 
 
-
-$app->get('what-to-expect', function() use ($app) {
+$app->get('/2015/what-to-expect', function() use ($app) {
     return page('what-to-expect');
 });
 
-$app->get('events', function() use ($app) {
-	dryfta('program-schedule');
+$app->get('/2015/events', function() use ($app) {
+	dryfta('/program-schedule');
 });
 
-$app->get('hotels-and-transit', function() use ($app) {
+$app->get('/2015/hotels-and-transit', function() use ($app) {
 	dryfta('14731931811/about-1473196711/hotels-amp-transit-1456530150/documents');
 });
 
-$app->get('scholarship', function() use ($app) {
+$app->get('/2015/scholarship', function() use ($app) {
     dryfta('buy-tickets');
 });
 
-$app->get('schedule', function() use ($app) {
-    dryfta('schedule');
-});
-
-$app->get('events-hackathon', function() use ($app) {
-    dryfta('schedule');
-});
-
-$app->get('online-events', function() use ($app) {
+$app->get('/2015/online-events', function() use ($app) {
     return page('live');
 });
 
-$app->get('speakers', function() use ($app) {
-    // Redirect the 2015 url to 2016 page
-    dryfta('speakers');
-});
-
-$app->get('tags', function(Request $request) use ($app) {
-    // Redirect the 2015 url to 2016 page
-    dryfta('schedule');
-});
-
-$app->get('talks', function(Request $request) use ($app) {
-    // Redirect the 2015 url to 2016 page
-    dryfta('schedule');
-});
-
-$app->get('live', function(Request $request) use ($app) {
+$app->get('/2015/live', function(Request $request) use ($app) {
     return page('live');
 });
+
 
 function page($view, $args = [])
 {

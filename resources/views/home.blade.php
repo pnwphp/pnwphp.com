37e76045f27a18833a14e55fdca9224ae6e377a8
Andrew Woods <atwoods1@gmail.com>
2017-03-28 13:46:48 -0700

    Improved organization of homepage content
    Moved the homepage content around to make better use of space.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,21 +21,82 @@
     <div class="title">PNWPHP Conference</div>
     <div class="container">
         <div class="row">
-            <div class="col-md-2">
-                <img src="/img/pnwphp.png" width="160" />
-            </div>
             <div class="col-md-6">
                 <p class="lead">
                 The <strong>Pacific Northwest PHP Conference</strong> is a
-                3-day event in the Pacific Northwest region of the
-                United States for PHP and Web developers. Our past conferences
-                have included <strong>world renown speakers</strong> from the PHP community, about a
-                <strong>wide range of topics</strong> &mdash; from APIs to unit
-                testing. <strong>Become a part</strong> of our upcoming 2017 conference &mdash;
-                <a href="<?php echo $cfpUrl; ?>">Submit your CFPs</a> to become a speaker.
+                3-day event in the Pacific Northwest region of the United
+                States for PHP and Web developers. Our past conferences have
+                included <strong>world renown speakers</strong> from the PHP
+                community, about a <strong>wide range of topics</strong>
+                &mdash; from APIs and CMS to unit testing and version control.
                 </p>
+
+                <p class="lead">
+                <em>Add your voice to the herd!</em> <strong>Become a part</strong> of our upcoming 2017 conference
+                &mdash; <a href="<?php echo $cfpUrl; ?>">Submit your CFPs</a>
+                to become a speaker.
+                </p>
+
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
+                <!--
+                        September 2017
+                    Su Mo Tu We Th Fr Sa
+                                    1  2
+                     3  4  5  6  7  8  9
+                    10 11 12 13 14 15 16
+                    17 18 19 20 21 22 23
+                    24 25 26 27 28 29 30
+                -->
+                <table class="table table-bordered">
+                    <tr>
+                        <th class="bg-info">Wed, Sep 6th</th>
+                        <td>
+                            <ul class="list-unstyled">
+                                <li><strong>Meetup</strong></li>
+                                <li><strong>Drinkup</strong></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="bg-info">Thu, Sep 7th</th>
+                        <td>
+                            <ul class="list-unstyled">
+                                <li><strong>Workshops</strong></li>
+                                <li><strong>Speaker Dinner</strong></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="bg-info">Fri, Sep 8th</th>
+                        <td>
+                            <ul class="list-unstyled">
+                                <li><strong>Conference</strong></li>
+                                <li><strong>Social</strong></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="bg-info">Sat, Sep 9th</th>
+                        <td>
+                            <ul class="list-unstyled">
+                                <li><strong>Conference</strong></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th class="bg-info">Sun, Sep 10th</th>
+                        <td>
+                            <ul class="list-unstyled">
+                                <li><strong>WurstConSEA</strong></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="row hidden-xs">
+            <div class="col-md-6">
                 <a href="https://twitter.com/PNWPHP" target="_blank" class="btn btn-lg btn-info btn-block">
                     <i class="fa fa-twitter"></i>&ensp;Follow @PNWPHP
                 </a>
@@ -43,50 +104,7 @@
                     <i class="fa fa-twitter"></i>&ensp;Follow @SeaPHP
                 </a>
             </div>
-        </div>
-        <div class="row hidden-xs">
-            <div class="col-md-8">
-                <table class="table table-bordered text-center">
-                    <tr class="bg-info">
-                        <th class="text-center">Sep 6th</th>
-                        <th class="text-center">Sep 7th</th>
-                        <th class="text-center">Sep 8th</th>
-                        <th class="text-center">Sep 9th</th>
-                        <th class="text-center">Sep 10th</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <ul class="list-unstyled text-center">
-                                <li><strong>Meetup</strong></li>
-                                <li><strong>Drinkup</strong></li>
-                            </ul>
-                        </td>
-                        <td>
-                            <ul class="list-unstyled text-center">
-                                <li><strong>Workshops</strong></li>
-                                <li><strong>Speaker Dinner</strong></li>
-                            </ul>
-                        </td>
-                        <td>
-                            <ul class="list-unstyled text-center">
-                                <li><strong>Conference</strong></li>
-                                <li><strong>Social</strong></li>
-                            </ul>
-                        </td>
-                        <td>
-                            <ul class="list-unstyled text-center">
-                                <li><strong>Conference</strong></li>
-                            </ul>
-                        </td>
-                        <td>
-                            <ul class="list-unstyled text-center">
-                                <li><strong>WurstConSEA</strong></li>
-                            </ul>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 &nbsp;
             </div>
         </div>
@@ -111,7 +129,7 @@
                     <img class="img-responsive img-thumbnail" src="img/impacthub.jpg" alt=""/>
                 </p>
                 <p class="lead">
-                    <a href="http://www.washington.edu/maps/#!/kne" target="_blank">University of Washington</a> is an excellent University, located in the heart of Seattle. The main conference will be held at <strong>Kane Hall</strong>, and the workshops in nearby <strong>Johnson Hall</strong>. 
+                    <a href="http://www.washington.edu/maps/#!/kne" target="_blank">University of Washington</a> is an excellent University, located in the heart of Seattle. The main conference will be held at <strong>Kane Hall</strong>, and the workshops in nearby <strong>Johnson Hall</strong>.
                 </p>
             </div>
         </div>

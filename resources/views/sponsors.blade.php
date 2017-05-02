e51f28b60c98a9f394e6ac067d52cdec469c39b5
Andrew Woods <atwoods1@gmail.com>
2017-05-02 12:13:49 -0700

    Simplify markup on sponsors template
    Still need to develop this template.

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -1,24 +1,18 @@
 @extends('layout')
 
 @section('content')
-    <div class="head-banner head-banner-sm">
-        <div class="bg"></div>
-    </div>
-    <div class="head-banner-content head-banner-content-sm">
-        <div>
-            <h1>Pacific Northwest PHP</h1>
-        </div>
-    </div>
 
-    <div class="wrapper wrapper-light">
-        <div class="title">Sponsors</div>
-        <div class="container">
-            <div class="col-md-8 col-md-offset-2">
-                <p class="lead text-center">
-                    Great sponsors help make successful event possible. If you're
-                    interested in sponsoring our upcoming 2017 conference on
-                    September 7-9, please get in touch with us.
-                </p>
+    <div class="wrapper wrapper-blue">
+        <div class="wrapper-content">
+            <h2>Sponsors</h2>
+            <div class="container">
+                <div class="">
+                    <p class="lead">
+                        Great sponsors help make successful event possible. If you're
+                        interested in sponsoring our upcoming 2017 conference on
+                        September 7-9, please get in touch with us.
+                    </p>
+                </div>
             </div>
         </div>
 

81668ab76921ca3fa2423b2e7b0f009ff1256a5f
Andrew Woods <atwoods1@gmail.com>
2017-06-06 13:08:31 -0700

    Reformat file
    I used the coding standard and my IDE
    and formatted code automatically
    to shift things around
    and align them a bit
    to increase it's quality

--- database/pnwphp-2017.php
+++ database/pnwphp-2017.php
@@ -1,30 +1,30 @@
 <?php return [
     'conference' => [
-        'title' => 'Pacific Northwest PHP',
-        'abbr' => 'PNWPHP',
+        'title'     => 'Pacific Northwest PHP',
+        'abbr'      => 'PNWPHP',
         'startDate' => new DateTime('2017-09-07T09:00:00+07:00'),
-        'endDate' => new DateTime('2017-09-09T19:00:00+07:00'),
+        'endDate'   => new DateTime('2017-09-09T19:00:00+07:00'),
     ],
-    'rooms' => [
+    'rooms'      => [
         'kne-120' => [
-            'name' => 'Kane Hall 120',
-            'code' => 'kne-120',
+            'name'     => 'Kane Hall 120',
+            'code'     => 'kne-120',
             'location' => 'University of Washington',
-            'floor' => '1st',
+            'floor'    => '1st',
             'capacity' => 400,
         ],
         'jhn-110' => [
-            'name' => 'Johnson 110',
-            'code' => 'jhn-110',
+            'name'     => 'Johnson 110',
+            'code'     => 'jhn-110',
             'location' => 'University of Washington',
-            'floor' => '1st',
+            'floor'    => '1st',
             'capacity' => 60,
         ],
         'jhn-170' => [
-            'name' => 'Johnson 170',
-            'code' => 'jhn-170',
+            'name'     => 'Johnson 170',
+            'code'     => 'jhn-170',
             'location' => 'University of Washington',
-            'floor' => '1st',
+            'floor'    => '1st',
             'capacity' => 60,
         ],
     ],
@@ -33,32 +33,32 @@
 
     'talks' => [],
 
-	'sponsor_levels' => [
-		'platinum' => [
-			'title' => 'Platinum Sponsor',
-			'level' => 'platinum'
-		],
-		'gold' => [
-			'title' => 'Gold Sponsors',
-			'level' => 'gold'
-		],
-		'silver' => [
-			'title' => 'Silver Sponsors',
-			'level' => 'silver'
-		],
-		'bronze' => [
-			'title' => 'Bronze Sponsors',
-			'level' => 'bronze'
-		],
-		'copper' => [
-			'title' => 'Copper Sponsors',
-			'level' => 'copper'
-		],
-		'community' => [
-			'title' => 'Community Sponsors',
-			'level' => 'community'
-		],
-	],
+    'sponsor_levels' => [
+        'platinum'  => [
+            'title' => 'Platinum Sponsor',
+            'level' => 'platinum'
+        ],
+        'gold'      => [
+            'title' => 'Gold Sponsors',
+            'level' => 'gold'
+        ],
+        'silver'    => [
+            'title' => 'Silver Sponsors',
+            'level' => 'silver'
+        ],
+        'bronze'    => [
+            'title' => 'Bronze Sponsors',
+            'level' => 'bronze'
+        ],
+        'copper'    => [
+            'title' => 'Copper Sponsors',
+            'level' => 'copper'
+        ],
+        'community' => [
+            'title' => 'Community Sponsors',
+            'level' => 'community'
+        ],
+    ],
 
     'sponsors' => [
         'platinum'  => [],
@@ -68,12 +68,12 @@
         'copper'    => ['cool-blue-web'],
         'community' => ['nomad-php'],
 
-	    'sample' => [
-		    'name' => 'Sample',
-		    'code' => 'sample',
-		    'url' => 'http://example.com/',
-		    'logo' => 'https://dummyimage.com/300x300/f2f2f2/3a3a3a.png&text=Sample',
-		    'about' => <<<DESC
+        'sample'        => [
+            'name'  => 'Sample',
+            'code'  => 'sample',
+            'url'   => 'http://example.com/',
+            'logo'  => 'https://dummyimage.com/300x300/f2f2f2/3a3a3a.png&text=Sample',
+            'about' => <<<DESC
 This could be your company. Get in touch with the organizers to become a sponsor.
 DESC
         ],
